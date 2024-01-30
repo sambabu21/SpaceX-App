81774aed02281bff46cd2d54428a26c39167f9f8
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-01-31 01:38:49 +0530

    Initial React app setup
    Implemented rockets section of the app

---
+++ src/Components/Rocket/Banner.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Banner(){
+    return (
+        <div className="banner-container">
+            <h1>SpaceX Rockets</h1>
+        </div>
+    )
+    } 
+    
---
+++ src/Components/Rocket/index.jsx
@@ -0,0 +1,45 @@
+
+import TileSection from './TileSection'
+import SearchForm from './SearchForm'
+import { useData } from '../../customHooks'
+import Banner from './Banner'
+
+
+export default function Rocket() {
+  
+  const { data,filteredResults,filters,setFilters } = useData('https://api.spacexdata.com/v3/rockets')
+  
+    function handleChange(e){
+        const { name,value } = e.target
+        setFilters((prevFilters)=>{
+            return {
+                ...prevFilters,
+                [name] : (value==="")?null:value
+            }
+        })
+    }
+
+    function clearFilters(){
+      setFilters({
+        rocket_name:'',
+        active:null,
+        country:null
+    })
+    }
+
+  return (
+    <div className='site-container'>
+      <Banner />
+      <SearchForm 
+          filters={filters} 
+          handleChange={handleChange}
+          clearFilters={clearFilters}
+        />
+      <TileSection 
+          rocketList={filteredResults}
+      />
+    </div>
+  )
+}
+
+
